8d0baae47a8a6c580b3ccb23aaa2706f33b2d336
NicoSimpsonPadilla <nicosimpsonpadilla@gmail.com>
2024-01-19 14:24:28 +0100

    nav y proyecto terminados

--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -8,9 +8,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
+    <nav class="navbar bg-body-tertiary">
+        <form class="container-fluid justify-content-start">
+            <a class="btn btn-dark me-2" href="{{ route('trains.index') }}">Trains</a>
+            <a class="btn btn-dark me-2" href="{{ route('train_types.index') }}">Train Types</a>
+            <a class="btn btn-dark me-2" href="{{ route('tickets.index') }}">Tickets</a>
+            <a class="btn btn-dark me-2" href="{{ route('ticket_types.index') }}">Ticket Types</a>
+        </form>
+    </nav>
     <div class="container">
         <div class="container center-block">
-            <h1>Train</h1>
+            <h1>Trains</h1>
         </div>
         <div class="container mb-2">
             <a class="btn btn-dark" href="{{ route('trains.create') }}">Crear tren</a>
